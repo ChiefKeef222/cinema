c95daab3be715ba9e340ea2d4ce58cd7785aa9db
ChiefKeef222 <aarhat144@gmail.com>
2025-10-10 15:25:26 +0500

    create RegisterViewSet

--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
 from rest_framework.permissions import AllowAny
 from rest_framework import viewsets
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializer import RegisterSerializer
 
 from .serializer import UserSerializer
 from .models import User
@@ -21,4 +26,22 @@ class UserViewSet(viewsets.ModelViewSet):
         return obj
 
 
-# class RegisterViewSet(ViewSet):
+class RegisterViewSet(ViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+    http_method_names = ["post"]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        res = {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+        return Response({
+            'user': serializer.data,
+            'refresh': str(refresh),
+            'token': res['access']
+        }, status=status.HTTP_201_CREATED)
