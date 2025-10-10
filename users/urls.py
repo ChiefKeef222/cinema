c016f79c9a0c24f04b798917d105093fd870d6c4
ChiefKeef222 <aarhat144@gmail.com>
2025-10-10 17:01:00 +0500

    create login feature

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, RegisterViewSet
+from .views import UserViewSet, RegisterViewSet, LoginViewSet
 
 router = DefaultRouter()
 router.register(r'', UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/register/', RegisterViewSet.as_view({'post': 'create'}), name='auth-register'),
 ]
 
--- users/views.py
+++ users/views.py
@@ -4,9 +4,9 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializer import RegisterSerializer
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 
-from .serializer import UserSerializer
+from .serializer import UserSerializer, RegisterSerializer, LoginSerializer
 from .models import User
 
 
@@ -46,3 +46,19 @@ class RegisterViewSet(ViewSet):
             'refresh': str(refresh),
             'token': res['access']
         }, status=status.HTTP_201_CREATED)
+
+class LoginViewSet(ViewSet):
+    serializer_class = LoginSerializer
+    permission_classes = (AllowAny, )
+    http_method_names = ['post']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
