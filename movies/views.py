a386bb39960559b41f463f4f01ad6a5cf43c1ea8
ChiefKeef222 <aarhat144@gmail.com>
2025-10-12 01:55:22 +0500

    create new app, wrote new views, wrote abstract view

--- movies/views.py
+++ movies/views.py
@@ -1,46 +1,13 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
-from users.permissions import IsAdmin
+from common.views import BaseCRUDViewSet
 
 from movies.serializer import MovieSerializer
 from movies.models import Movie
 
-class MovieViewSet(viewsets.ModelViewSet):
+
+class MovieViewSet(BaseCRUDViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    lookup_field = "public_id"
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get_permissions(self):
-        if self.action in ["create", "update", "partial_update", "destroy"]:
-            self.permission_classes = [IsAdmin]
-        else:
-            self.permission_classes = [IsAuthenticatedOrReadOnly]
-        return super().get_permissions()
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response(
-            {
-                "message": "Фильм успешно создан",
-                "id": str(serializer.instance.public_id),
-            },
-            status=status.HTTP_201_CREATED,
-        )
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop("partial", False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response({"message": "Фильм успешно обновлён"})
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response({"message": "Фильм успешно удалён"})
+    message_create = "Фильм успешно создан"
+    message_update = "Фильм успешно обновлён"
+    message_destroy = "Фильм успешно удалён"
--- schedule/serializer.py
+++ schedule/serializer.py
@@ -1,3 +1,37 @@
-# from rest_framework import serializers
-#
-# from .models import Hall, Session
+from rest_framework import serializers
+
+from .models import Hall, Session
+from movies.models import Movie
+
+class HallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hall
+        fields = [
+            'public_id',
+            'name',
+            'rows',
+            'seats_per_row'
+        ]
+        read_only_fields = ['public_id']
+
+
+class SessionSerializer(serializers.ModelSerializer):
+    movie_id = serializers.SlugRelatedField(
+        slug_field='public_id',  # связываем по UUID фильма
+        queryset=Movie.objects.all()
+    )
+    hall_id = serializers.SlugRelatedField(
+        slug_field='public_id',  # связываем по UUID зала
+        queryset=Hall.objects.all()
+    )
+
+    class Meta:
+        model = Session
+        fields = [
+            'public_id',
+            'movie_id',
+            'hall_id',
+            'start_time',
+            'price',
+        ]
+        read_only_fields = ['public_id']
