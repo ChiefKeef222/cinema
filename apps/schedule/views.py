91d836d391e8122443386c9e2908885590b4393f
ChiefKeef222 <aarhat144@gmail.com>
2025-10-19 00:09:34 +0500

    optimized the query for HallViewSet

--- apps/schedule/views.py
+++ apps/schedule/views.py
@@ -1,18 +1,29 @@
 from apps.common.viewsets import BaseCRUDViewSet
 from django_filters import rest_framework as filters
+from django.db.models import Prefetch
 
 from .serializer import HallSerializer, SessionSerializer
-from .models import Session, Hall
+from .models import Session, Hall, Seat
 
 
 
 class HallViewSet(BaseCRUDViewSet):
-    queryset = Hall.objects.all()
+    # queryset = Hall.objects.all()
     serializer_class = HallSerializer
 
     message_create = "Зал успешно создан"
     message_update = "Зал успешно обновлён"
     message_destroy = "Зал успешно удалён"
+
+    def get_queryset(self):
+        return (Hall.objects.prefetch_related(Prefetch(
+                "seats",
+                queryset=Seat.objects.order_by("row_number", "seat_number")
+            )
+        )
+        .all())
+
+
 
 class SessionFilter(filters.FilterSet):
     movie = filters.UUIDFilter(field_name='movie_id__public_id')
