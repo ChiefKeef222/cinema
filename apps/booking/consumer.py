ea43ea4e32ad6a51940b57d3c64428b190f54c07
ChiefKeef222 <aarhat144@gmail.com>
2025-10-30 16:52:05 +0500

    update consumer file

--- apps/booking/consumer.py
+++ apps/booking/consumer.py
@@ -7,15 +7,14 @@ class SeatConsumer(AsyncWebsocketConsumer):
         self.session_id = self.scope["url_route"]["kwargs"]["session_id"]
         self.group_name = f"session_{self.session_id}"
 
-        # Присоединяемся к группе
+
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Покидаем группу
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    # Получение сообщений от группы
+
     async def seat_update(self, event):
         await self.send(
             text_data=json.dumps(
